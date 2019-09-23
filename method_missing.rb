a70214bcc460671abda4e70bd671ee3f4061a50d
FlevianK <flevian.kanaiza@andela.com>
2019-09-23 18:51:15 +0300

    Add method missing algorithm

---
+++ method_missing.rb
@@ -0,0 +1,43 @@
+# Using method_missing
+class Presenter
+  attr_accessor :object
+
+  def initialize(object)
+    self.object = object
+  end
+
+  # If a method we call is missing, pass the call onto 
+  # the object we delegate to.
+  def method_missing(m, *args, &block)
+    puts "Delegating #{m}"
+    object.send(m, *args, &block)
+  end
+end
+
+class UserPresenter < Presenter
+
+  # We just want to display the first letter of the last name
+  def last_name
+    "#{object.last_name[0]}."
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+end
+
+# A mini User object to work with
+User = Struct.new(:first_name, :last_name, :age)
+user = User.new("Leigh", "Halliday", 30)
+user_presenter = UserPresenter.new(user)
+
+puts user_presenter.full_name
+puts user_presenter.age
+
+# Delegating first_name
+# Leigh H.
+# Delegating age
+# 30
+
+# Re-Creating the Alias method
